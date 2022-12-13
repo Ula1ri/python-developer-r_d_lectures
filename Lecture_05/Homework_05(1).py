27b7515c9faa8c5f6e0f8a3201b05d560ba607cf
Ula1ri <119409591+Ula1ri@users.noreply.github.com>
2022-12-13 20:16:52 +0100

    Homewok to 5th lecture

---
+++ Lecture_05/Homework_05(1).py
@@ -0,0 +1,21 @@
+# 1. Створити програму, яка буде очікувати від користувача введення тексту 
+# і виведе інформацію по кожному надрукованому символу: 
+# це “число” + яке воно (парне, непарне), 
+# це “буква” + яка вона (велика чи маленька),
+# це “символ”
+
+s = input('Write me something, please: ')
+z = list(s)
+for item in z:
+    if item.isdigit():
+        item = int(item)
+        if item % 2 == 0:
+            print(f'{item} - this number is even')
+        else:
+            print(f'{item} - this number is odd')
+    elif item.isupper():
+        print(f'{item} - this letter is in upper case')
+    elif item.islower():
+        print(f'{item} - this letter is in lower case')
+    else:
+        print(f'{item} - this is symbol')
