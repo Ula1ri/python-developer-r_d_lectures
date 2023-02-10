61dce0f0fe29abc8213b62065b6470fbd4635bdc
OLEKSANDR OROBCHUK <pohanice@gmail.com>
2023-02-10 17:37:36 +0100

    update 3
    Прошу вибачення за купу помилок, код тепер виконується але є питання. Щоб він спрацьовував я вказую в команді some_bot.say_name() в дужках ім'я классу. В іншому випадку   інтерпритатор видає помилку. Це так має бути, чи я щось роблю не так? Дякую за терпіння!

--- Lecture_18/Homework_18.py
+++ Lecture_18/Homework_18.py
@@ -1,5 +1,5 @@
 class Bot:
-    def __init__(self, name,):
+    def __init__(self, name):
         self.name = name
     def say_name(self, name):
         print(self.name)
@@ -12,22 +12,26 @@ class TelegramBot(Bot):
         super().__init__(name)
         self.url = url
         self.chat_id = chat_id
-    def send_massage(self, message):
-        super().send_message()
-        return TelegramBot(self.send_message())
+    def send_message(self, message):
+        print(f'{self.name} bot says {message} to chat {self.chat_id} using {self.url}')
+        
     def set_url(self, url):
         self.url = url
         print(url)
     def set_chat_id(self, chat_id):
         self.chat_id = chat_id
-        print(f'TG bot says{message} to chat {self.chat_id} using {self.url}')
 
 some_bot = Bot('Marvin')
-some_bot.say_name()
-some_bot.send_message("Hello")
 
-telegram_bot = TelegramBot('TG')
-telegram_bot.say_name()
+some_bot.say_name(Bot)
+
+some_bot.send_message("Hello")
+telegram_bot = TelegramBot("TG")
+
+telegram_bot.say_name(TelegramBot)
+
 telegram_bot.send_message('Hello')
+
 telegram_bot.set_chat_id(1)
+
 telegram_bot.send_message('Hello')
