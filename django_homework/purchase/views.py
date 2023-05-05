7ed9abde402020f29514e107468dc71932cd4c59
OLEKSANDR OROBCHUK <pohanice@gmail.com>
2023-05-05 17:56:22 +0200

    django rest framework with Viewset and filtering

--- django_homework/purchase/views.py
+++ django_homework/purchase/views.py
@@ -1,6 +1,11 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+
 from .models import Purchase
 from django.views.generic import ListView, CreateView, DetailView
+from rest_framework.viewsets import ModelViewSet
 from .forms import PurchaseForm
+from .serializers import PurchaseSerializer
 
 
 class PurchaseListView(ListView):
@@ -15,3 +20,11 @@ class PurchaseCreateView(CreateView):
     model = Purchase
     form_class = PurchaseForm
     success_url = '/purchase'
+
+
+class PurchaseViewSet(ModelViewSet):
+    queryset = Purchase.objects.all()
+    serializer_class = PurchaseSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['id', 'date', 'book_id_id', 'user_id_id']
+    search_fields = ['id', 'date', 'book_id_id', 'user_id_id']
